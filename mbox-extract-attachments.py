49be427cb4ef4bafd6de30541ccc822786de5a56
Liam Bowen <LiamBowen@gmail.com>
2016-03-09 19:11:13 -0800

    remove outdated docs

--- mbox-extract-attachments.py
+++ mbox-extract-attachments.py
@@ -1,42 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# mbox-extract-attachments.py - Extract attachments from mbox files
-# 16/March/2012
-# Copyright (C) 2012 Pablo Castellano <pablo@anche.no>
-#
-# Adapted by Liam Bowen <LiamBowen@gmail.com>
-# November 2015
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-
-# Notes (RFC 1341):
-# The use of a Content-Type of multipart in a body part within another
-# multipart entity is explicitly allowed. In such cases, for obvious reasons,
-# care must be taken to ensure that each nested multipart entity must use a
-# different boundary delimiter. See Appendix C for an example of nested
-# multipart entities.
-# The use of the multipart Content-Type with only a single body part may be
-# useful in certain contexts, and is explicitly permitted.
-# The only mandatory parameter for the multipart Content-Type is the boundary
-# parameter, which consists of 1 to 70 characters from a set of characters
-# known to be very robust through email gateways, and NOT ending with white
-# space. (If a boundary appears to end with white space, the white space must
-# be presumed to have been added by a gateway, and should be deleted.) It is
-# formally specified by the following BNF
-
-# Related RFCs: 2047, 2044, 1522
 
 from __future__ import print_function
 
@@ -52,6 +15,7 @@ import os
 import sys
 import logging
 import fnmatch
+import hashlib
 try:
     from tqdm import tqdm
 except ImportError:
@@ -74,7 +38,6 @@ def extract_attachment(msg, destination):
     orig_destination = destination
     n = 1
     while os.path.exists(destination):
-        # TODO: Detect if it's just a duplicate
         destination = orig_destination + "." + str(n)
         n += 1
 
